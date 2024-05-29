5a58c40e35974d37182aa13cc7f232f58b8e1baa
M4RKL4B0Y <markvlaboy@gmail.com>
2024-05-28 20:36:47 -0400

    declare typescript

---
+++ types/index.d.ts
@@ -0,0 +1,5 @@
+declare module 'lodash' {
+    function multiply(num1: number, num2: number): number
+    function reverse<T>(arr: T[]): T[]
+    function dropRight<T>(arr: any[], num: number): any[]
+}
